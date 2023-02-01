30f313b5550abd8f3a92f50287145a81c8f99fed
Aich <auch.aziz@gmail.com>
2023-02-01 15:01:27 +0100

    start

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+function Footer() {
+    return (
+      <footer>
+            <img src={ll} alt="little lemon logo" />
+            <nav>
+                <ul>
+                 <li><a href="#">Home</a></li>
+                 <li><a href="#">Home</a></li>
+                 <li><a href="#">Home</a></li>
+                 <li><a href="#">Home</a></li>
+                </ul>
+            </nav>
+      </footer>
+    );
+  }
+  export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import ll from "../assets/logo.jpg"
+function Header() {
+    return (
+      <header>
+            <img src={ll} alt="little lemon logo" />
+            <nav>
+                <ul>
+                 <li><a href="#">Home</a></li>
+                 <li><a href="#">Home</a></li>
+                 <li><a href="#">Home</a></li>
+                 <li><a href="#">Home</a></li>
+                </ul>
+            </nav>
+      </header>
+    );
+  }
+  export default Header;
