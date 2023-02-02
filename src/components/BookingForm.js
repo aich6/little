3516ee5f959887208e6a234ffb2da028706380b7
Aich <auch.aziz@gmail.com>
2023-02-02 11:48:02 +0100

    v4

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -6,13 +6,18 @@ function BookingForm({avTime, dispatch,submitForm}) {
     const [occasion, setOccasion] = useState('')
     const [date,setDate] = useState('')
     const [time,setTime] = useState('')
+    const [valid,setValid] = useState(false)
+    
     const handleChange = (e)=> {
       setDate(e.target.value)
       const validDate = new Date(date)
       dispatch({type: 'addDate',payload:validDate})
     }
-
+    const validate = () => {
+      return  date.length && time.length ;
+    };
     const handleSubmit = (e) => {
+      
         e.preventDefault();
         submitForm({
           guest: guests,
@@ -21,14 +26,13 @@ function BookingForm({avTime, dispatch,submitForm}) {
           occasion: occasion
         })
     }
- 
-
+    
     return (
       <>
         <h1>Book Now</h1>
         <form onSubmit={handleSubmit}>
         <label htmlFor="res-date">Choose date</label>
-        <input value={date} onChange={handleChange} type="date" id="res-date" />
+        <input required value={date} onChange={handleChange} type="date" id="res-date" />
         <label htmlFor="res-time">Choose time</label>
         <select value={time} onChange={(e)=> setTime(e.target.value)} id="res-time ">
         {avTime && avTime.map((project) => (
@@ -42,7 +46,7 @@ function BookingForm({avTime, dispatch,submitForm}) {
            <option>Birthday</option>
            <option>Anniversary</option>
         </select>
-        <input type="submit" value="Make Your reservation" />
+        <input aria-label="On Click" disabled={!validate()} type="submit" value="Make Your reservation" />
      </form> 
      </>
     );
